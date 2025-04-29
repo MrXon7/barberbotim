ae528388af75ad40642e363310e270f3c0795c2a
MrXon7 <mrxon73@gmail.com>
2025-04-29 14:00:13 +0500

    keyboardni ishlatish

--- main.py
+++ main.py
@@ -126,11 +126,11 @@ dp = Dispatcher()
 def make_web_keyboard(user_id: int) -> Optional[ReplyKeyboardMarkup]:
     """WebApp tugmalari bilan klaviatura yaratish"""
     try:
-        
-        keyboard=[KeyboardButton(
+        keyboard=[]
+        keyboard.append(KeyboardButton(
                 text="Mening Sartaroshim",
                 web_app=WebAppInfo(url=f"https://barber-queue-c0c6f.web.app?telegram_id={user_id}")
-            )]
+            ))
       
         return ReplyKeyboardMarkup(
             keyboard=keyboard,
